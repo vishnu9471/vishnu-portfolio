7f379f23f7da5a50e621f76b46af510fb1835aae
vishnu9418 <kantpandeyvishnu312@gmail.com>
2026-08-16 20:09:55 +0530

    Fix CORS for Vercel frontend

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:5173",
   "https://vishnu-portfolio-one-bice.vercel.app",
+  "https://vishnu-portfolio-l4enyuf98-vishnu9471s-projects.vercel.app",
 ];
 
 app.use(
